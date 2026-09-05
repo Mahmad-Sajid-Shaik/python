e63cacb7afade9276dfb316479b695a259ebf484
Sajid <sajid5541shaik@gmail.com>
2026-09-05 16:45:11 +0530

    Numeric Variables

---
+++ Day03.py
@@ -0,0 +1,25 @@
+# Numeric variables
+a = 10
+b = 20
+
+# Swapping
+a, b = b, a
+
+# String
+name = "Sajid"
+
+# List
+marks = [80, 85, 90]
+
+# Tuple
+student = ("Sajid", "ECE", 23)
+
+# Range
+numbers = list(range(1, 6))
+
+print("a =", a)
+print("b =", b)
+print("Name =", name)
+print("Marks =", marks)
+print("Student =", student)
+print("Range =", numbers)
