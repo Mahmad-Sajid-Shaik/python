e73e170f90239efc4fe518e8c689580c09246977
Sajid <sajid5541shaik@gmail.com>
2026-09-05 16:38:04 +0530

    variable

---
+++ Day02.py
@@ -0,0 +1,21 @@
+# Variable creation
+name = "Sajid"
+age = 23
+
+# Multiple assignment
+city, country = "Hyderabad", "India"
+
+print(name)
+print(age)
+print(city)
+print(country)
+
+# Reassignment
+age = 24
+
+print("New age:", age)
+
+# Deleting variable
+del country
+
+print(city)
